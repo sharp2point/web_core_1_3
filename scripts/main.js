6283b8c01517102e2eb5c043bb2128e68fa93fe2
Blinov Evgeny <sharppoint@yandex.ru>
2022-09-15 15:35:22 +0300

    исправленна работа модального меню

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,26 @@
 const burger_button = document.querySelector(".upper-menu__button-burger");
-const mobile_menu = document.querySelector(".mobile-menu");
-const mobile_menu_close_button = document.querySelector(".mobile-menu__button-close");
+const modal_host = document.querySelector(".modal-host");
+const mobile_menu_close_button = document.querySelector(
+  ".mobile-menu__button-close"
+);
 
-burger_button.addEventListener("click",(e)=>{
-    mobile_menu.classList.add("mobile-menu_open")
-    mobile_menu_close_button.addEventListener("click",(e)=>{
-        mobile_menu.classList.remove("mobile-menu_open")
-    })
-})
+function open_modal(is_open) {
+  if (is_open) {
+    modal_host.classList.add("modal-host_open");
+  } else {
+    modal_host.classList.remove("modal-host_open");
+  }
+}
+
+// открывает модальное окно по клику на кнопке burger-button
+burger_button.addEventListener("click", (e) => open_modal(true));
+
+// закрывает модальное окно по клику на пустом пространстве модального окна
+modal_host.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal-host")) {
+    open_modal(false);
+  }
+});
+
+// закрывает модальное окно по клику на кнопке close-button
+mobile_menu_close_button.addEventListener("click", (e) => open_modal(false));
